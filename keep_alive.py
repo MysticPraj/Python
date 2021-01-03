208bbf5d85648767c0a516074dc50bb8397b10e1
Prajwal Gangawane <46399757+MysticPraj@users.noreply.github.com>
2021-01-04 00:19:11 +0530

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+  app.run(host='github.com')
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
